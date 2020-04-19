9202d827288810429a1076f49dbda9547a58f149
Yogesh S. Shisode <yogesh13.sh@gmail.com>
2020-04-19 17:31:06 +0530

    Update response of the API in JSON format

--- src/services/schemavalidator.js
+++ src/services/schemavalidator.js
@@ -5,7 +5,21 @@ module.exports = {
         let instance = req.body.instance;
         let schema = req.body.schema;
 
-        let response = validate(instance, schema);
+        let result = validate(instance, schema);
+
+        let response = {};
+
+        if(result.errors != null && result.errors.length > 0){
+            response.status = 1001;
+            response.message = "JSON request validation failed against provided JSON schema";
+            response.body = result.errors;
+
+        }else{
+            response.status = 1000;
+            response.message = "JSON request validated successfully against provided JSON schema";
+            response.body = "";
+        }
+
         callback(response);
     }
 }
